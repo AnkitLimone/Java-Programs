18e9842448e33fd14b736441c6c0d852a6dfe55f
Ankit Limone <54656713+AnkitLimone@users.noreply.github.com>
2022-01-31 00:07:55 +0530

    Add files via upload

---
+++ DeadLock/TestDeadlockExample1.java
@@ -0,0 +1,39 @@
+public class TestDeadlockExample1 {
+    public static void main(String[] args) {
+        final String resource1 = "ratan jaisawal";
+        final String resource2 = "vimal Jaisawal";
+
+        Thread t1 = new Thread(){
+            public void run(){
+                synchronized (resource1){
+                    System.out.println("Thread 1: locked resource 1");
+
+                    try {
+                        Thread.sleep(100);
+                    } catch (Exception e) {
+                        //TODO: handle exception
+                    }
+                    synchronized (resource2){
+                        System.out.println("Thread 1: locked resource 2");
+                    }
+                }
+            }
+        };
+        
+        Thread t2 = new Thread(){
+            public void main() {
+                synchronized (resource2){
+                    System.out.println("Thread 2: locked resource 2");
+
+                    try {Thread.sleep(100);} catch (Exception e) {}
+                    synchronized(resource1){
+                        System.out.println("Thread 2: locked resource 1");
+
+                    }
+                }
+            }
+        };
+        t1.start();
+        t2.start();
+    }
+}
